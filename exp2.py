fd58586f6699ec727536d64aa95bd5e26a26fc8b
JenishDesai5115 <155424173+JenishDesai5115@users.noreply.github.com>
2026-08-13 12:19:10 +0530

    Add files via upload

---
+++ exp2.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cross_correlation(signal1, signal2):
+    return np.correlate(signal1, signal2, mode='full')
+
+def autocorrelation(signal):
+    return np.correlate(signal, signal, mode='full')
+
+signal1 = np.array([1, 2, 3, 4, 5])
+signal2 = np.array([2, 4, 6, 8, 10])
+
+cross_corr = cross_correlation(signal1, signal2)
+auto_corr = autocorrelation(signal1)
+
+plt.figure(figsize=(10, 6))
+
+plt.subplot(2, 1, 1)
+plt.stem(cross_corr)
+plt.title('Cross-Correlation')
+plt.xlabel('Time Lag')
+plt.ylabel('Magnitude')
+
+plt.subplot(2, 1, 2)
+plt.stem(auto_corr)
+plt.title('Autocorrelation')
+plt.xlabel('Time Lag')
+plt.ylabel('Magnitude')
+
+plt.tight_layout()
+plt.show()
